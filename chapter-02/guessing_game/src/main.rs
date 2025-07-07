5258e69887e9c116692efc12e0782a7589bf2309
Paulo Abreu <prrar83@gmail.com>
2025-07-07 10:45:37 -0300

    Chapter 3

--- chapter-02/guessing_game/src/main.rs
+++ chapter-02/guessing_game/src/main.rs
@@ -4,17 +4,37 @@
  */
 
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Your guess: ");
+    let secret_number = rand::rng().random_range(1..=10);
 
-    let mut guess = String::new();
+    println!("Secret number {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Your guess: ");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("CORRECT!");
+                break;
+            }
+        }
+    }
 }
